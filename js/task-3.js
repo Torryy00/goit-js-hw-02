cc9e91718c4d6b41d5cf240cc2bb06b766f1752c
tory <votakmasha@gmail.com>
2025-03-20 15:26:11 +0100

    commit masage

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,3 @@
-function getElementWidth(content, padding, border) {
-  const contentNum = parseFloat(content);
-  const paddingNum = parseFloat(padding);
-  const borderNum = parseFloat(border);
-  return contentNum + 2 * paddingNum + 2 * borderNum;
-}
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
-
 function checkForSpam(message) {
 const messageLowerCase = message.toLowerCase();
 if (messageLowerCase.includes("spam") || messageLowerCase.includes("sale")) {
